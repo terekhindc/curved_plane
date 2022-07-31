62b690146046a934a7d81b808054d55690a735f6
vr-corp-ru <103163597+vr-corp-ru@users.noreply.github.com>
2022-07-31 13:46:27 +0300

    updated uvs

--- Assets/TerekhinDT/Curved Plane/Components/CurvedPlane.cs
+++ Assets/TerekhinDT/Curved Plane/Components/CurvedPlane.cs
@@ -2,88 +2,43 @@ using UnityEngine;
 
 namespace TerekhinDT.Curved_Plane.Components
 {
+    /// <summary>
+    /// Procedural generation of curved plane
+    /// </summary>
     public class CurvedPlane : MonoBehaviour
     {
         public float diameter = 1;
-        public float width = 1;
+        public float width = 1.57f;
         public float height = 1f;
-        public int edgesCount = 360;
+        public int edgesCount = 30;
         private int _pointsCount;
 
-        private GameObject [] _verticesGo;
         private Vector3[] _vertices;
         private int[] _triangles;
 
-        private MeshFilter _meshFilter;
+        private MeshFilter MeshFilter => GetComponent<MeshFilter>();
         private Mesh _mesh;
 
-        private GameObject _testObj;
-
-        public bool isTesting;
-        public bool isVisualization;
-        
-        private void Awake()
+        private void Start ()
         {
             _pointsCount = (edgesCount + 1) * 2;
             _vertices = new Vector3[_pointsCount];
             
             _mesh = new Mesh();
-            _meshFilter = GetComponent<MeshFilter>();
             
-            if (isVisualization) InitVizObjects();
-            if (isTesting) InitTesting();
-        }
-
-        private void InitVizObjects()
-        {
-            _verticesGo = new GameObject[_pointsCount];
-            
-            for (var i = 0; i < _pointsCount; i++)
-            {
-                _verticesGo[i] = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                _verticesGo[i].transform.localScale = Vector3.one * 0.1f;
-            }
-        }
-
-        private void InitTesting()
-        {
-            _testObj = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-            _testObj.transform.localScale = Vector3.one * 0.3f;
-            _testObj.GetComponent<MeshRenderer>().material.color = Color.red;
-        }
-
-        private void Testing()
-        {
-            var r = diameter / 2;
-            var center = transform.position;
-            
-            var pos = new Vector3
-            {
-                x = Mathf.Cos(width/r*-1)*r,
-                y = height/2,
-                z = Mathf.Sin(width/r)*r
-            };
-
-            _testObj.transform.position = pos;
-        }
-
-        private void Start()
-        {
             Create();
         }
 
-        private void Update()
-        {
-            if (isTesting) Testing();
-        }
 
+        /// <summary>
+        /// Creation geometry
+        /// </summary>
         private void Create()
         {
-            // drawing vertices
             var r = diameter / 2;
             var center = transform.position;
             var delta = (width / r)/edgesCount;
-            
+
             for (var i = 0; i < _pointsCount; i++)
             {
 
@@ -92,19 +47,14 @@ namespace TerekhinDT.Curved_Plane.Components
                 var pos = new Vector3
                 {
                     x = Mathf.Cos(angle)*r,
-                    y = i % 2 == 0 ? center.y - height / 2 : center.y + height / 2,
-                    z = Mathf.Sin(angle)*r
+                    y = i % 2 == 0 ? center.y - height/2 : center.y + height/2,
+                    z = Mathf.Sin(angle*-1)*r
                 };
 
                 _vertices[i] = pos;
-
-                if (isVisualization)
-                {
-                    _verticesGo[i].transform.position = pos;
-                }
             }
             
-            //drawing triangles
+            //Drawing triangles
             _triangles = new int[edgesCount*2*3];
 
             var j = 0;
@@ -112,20 +62,28 @@ namespace TerekhinDT.Curved_Plane.Components
             for (var i = 0; i < edgesCount * 2 * 3; i += 6, j += 2)
             {
                 _triangles[i] = j;
-                _triangles[i+1] = j+1;
+                _triangles[i+1] = j+2;
                 _triangles[i+2] = j+3;
                 
                 _triangles[i+3] = j+3;
-                _triangles[i+4] = j+2;
+                _triangles[i+4] = j+1;
                 _triangles[i+5] = j;
                 
             }
             
             // UV settings
             var uvs = new Vector2[_vertices.Length];
-            for (var i = 0; i < uvs.Length; i++)
+            var step = (1.0f / _vertices.Length);
+
+            for (var i = 0; i < uvs.Length; i+=2)
             {
-                uvs[i] = _vertices[i];   
+                var x = 1 - (step * i);
+                
+                var pos1 = new Vector2(x, 0);
+                var pos2 = new Vector2(x, 1);
+                
+                uvs[i] = pos1;
+                uvs[i+1] = pos2;
             }
             
             // Set values in MeshFilter
@@ -139,8 +97,8 @@ namespace TerekhinDT.Curved_Plane.Components
             _mesh.RecalculateBounds();
             _mesh.RecalculateTangents();
             
-            _mesh.name = "Curved plane";
-            _meshFilter.mesh = _mesh;
+            _mesh.name = "CurvedPlane";
+            MeshFilter.mesh = _mesh;
         }
     }
 }
